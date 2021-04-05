49e27d33c8564711b9621e6ab8ec87921e4dbcc3
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-04-05 15:57:16 +0200

    setup apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 };
 
 export default MyApp;
