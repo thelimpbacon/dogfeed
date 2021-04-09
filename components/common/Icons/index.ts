03edb7b95ca24049166f4776567a73e6584fd4d2
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-04-09 15:31:23 +0200

    export icons from left side bar to own components

--- components/common/Icons/index.ts
+++ components/common/Icons/index.ts
@@ -3,3 +3,5 @@ export { default as Home } from "./Home";
 export { default as Light } from "./Light";
 export { default as Profile } from "./Profile";
 export { default as Users } from "./Users";
+export { default as Posts } from "./Posts";
+export { default as Settings } from "./Settings";
