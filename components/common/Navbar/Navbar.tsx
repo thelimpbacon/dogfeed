8f51bf256c441db6be88d01356739a8a27116f0d
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-04-10 11:29:50 +0200

    add font for logo change logo and site name to Dog Feed

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import { DarkModeToggle } from "..";
 import s from "./Navbar.module.css";
+import cn from "classnames";
 
 const Navbar = () => {
   return (
     <div className={s.root}>
       <Link href="/">
-        <a>Navbar</a>
+        <a>
+          <h2 className={cn(s.siteName, "dark:text-gray-50")}>Dog Feed</h2>
+        </a>
       </Link>
       <div className="ml-auto">
         <DarkModeToggle />
